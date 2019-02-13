c0679dc54c6d19d0eed796b25fa320a1cfed7074
“Yuan <yuantian013@163.com>
2019-02-13 20:27:36 +0100

    update

--- PPO.py
+++ PPO.py
@@ -1,17 +1,20 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import gym
-from cartpole_uncertainty import CartPoleEnv_adv as dreamer
-MAX_EPISODES = 2000
-MAX_EP_STEPS =1000
+from Q_ENV import QUADROTOR as QUADROTOR
+from mpl_toolkits.mplot3d import Axes3D
+MAX_EPISODES = 20000
+MAX_EP_STEPS =2500
 GAMMA = 0.99
 A_LR = 0.0001
 C_LR = 0.0002
 BATCH = 128
 A_UPDATE_STEPS = 10
 C_UPDATE_STEPS = 10
-S_DIM, A_DIM = 4, 1
+S_DIM, A_DIM = 13, 4
 
 EWMA_p=0.95
 EWMA_step=np.zeros((1,MAX_EPISODES+1))
@@ -26,10 +29,10 @@ METHOD = [
 
 class PPO(object):
 
-    def __init__(self):
+    def __init__(self,high):
         self.sess = tf.Session()
         self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
-
+        self.a_high = high
         # critic
         with tf.variable_scope('critic'):
             n_l1 = 256  # 30
@@ -45,8 +48,8 @@ class PPO(object):
             self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
 
         # actor
-        pi, pi_params = self._build_anet('pi', trainable=True)
-        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
+        pi, pi_params,self.mu,self.sigma = self._build_anet('pi', trainable=True)
+        oldpi, oldpi_params,_,_ = self._build_anet('oldpi', trainable=False)
         with tf.variable_scope('sample_action'):
             self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
         with tf.variable_scope('update_oldpi'):
@@ -106,49 +109,86 @@ class PPO(object):
             net_1 = tf.layers.dense(net_0, 256, activation=tf.nn.relu, name='l2', trainable=trainable)  # 原始是30
             net_2 = tf.layers.dense(net_1, 256, activation=tf.nn.relu, name='l3', trainable=trainable)  # 原始是30
             net_3 = tf.layers.dense(net_2, 128, activation=tf.nn.relu, name='l4', trainable=trainable)  # 原始是30
-            # l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu, trainable=trainable)
+
             mu = tf.layers.dense(net_3, A_DIM, activation=tf.nn.tanh, name='a', trainable=trainable)
+            # mu = tf.multiply(mu,self.a_high, name='scaled_a')
+
             sigma = tf.layers.dense(net_3, A_DIM, tf.nn.softplus, trainable=trainable)
-            norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
+            # sigma = tf.multiply(sigma, 0, name='scaled_sigma')
+            norm_dist= tf.distributions.Normal(loc=mu, scale=sigma)
+
         params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
-        return norm_dist, params
+        return norm_dist, params,mu,mu
 
     def choose_action(self, s):
         s = s[np.newaxis, :]
         a = self.sess.run(self.sample_op, {self.tfs: s})[0]
-        return np.clip(a, -20, 20)
+        # print(self.sess.run(self.mu, {self.tfs: s})[0])
+        return a#np.clip(a, a_low, a_high)
 
     def get_v(self, s):
         if s.ndim < 2: s = s[np.newaxis, :]
         return self.sess.run(self.v, {self.tfs: s})[0, 0]
 
 # ENV_NAME = 'CartPole-v2'
-env = dreamer()
+env = QUADROTOR()
 # env = gym.make(ENV_NAME)
 env = env.unwrapped
-
-ppo = PPO()
+a_high = env.force_space.high
+a_low = env.force_space.low
+ppo = PPO(a_high)
 all_ep_r = []
-
+fig = plt.figure()
+ax = plt.subplot(projection='3d')  # 创建一个三维的绘图工程
+plot_x = []
+plot_y = []
+plot_z = []
+fig = plt.figure()
+var=0.5*a_high
 for i in range(MAX_EPISODES):
-    s = env.reset()
+    ax = plt.subplot(projection='3d')  # 创建一个三维的绘图工程
+    ax.scatter(plot_x, plot_y, plot_z, c='r')  # 绘制数据点,颜色是红色
+    ax.set_zlabel('Z')  # 坐标轴
+    ax.set_ylabel('Y')
+    ax.set_xlabel('X')
+    plt.draw()
+    plt.pause(0.1)
+    plt.close(fig)
+    plot_x = []
+    plot_y = []
+    plot_z = []
+    # des_start = env.trajectory('circle', 0)
+    # s = env.reset_traj(des_start[0], 0)
+    s=env.reset()
     buffer_s, buffer_a, buffer_r = [], [], []
     ep_reward = 0
     for j in range(MAX_EP_STEPS):    # in one episode
-        train=0
-        env.render()
+        # print(s)
+        # if i>10:
+        print(s)
         a = ppo.choose_action(s)
-        # a = np.clip(np.random.normal(a, 5), -20, 20)  # add randomness to action selection for exploration
-        s_, r, done, hit = env.step(a)
+
+        print(a)
+        a = np.clip(a, a_low, a_high)
+        a[0]=a[0]+np.random.normal(a[0], a_high[0])
+        a = np.clip(np.random.normal(a, var), a_low, a_high)
+        # print(a)
+
+        # if i<2:
+        #     desired_state = env.trajectory('circle', t)
+        #     F, M = env.controller(desired_state, env.state)
+        #     a = [F, M[0], M[1], M[2]]
+        s_, r, done= env.ppo_step(a[0],a[1:])
         buffer_s.append(s)
         buffer_a.append(a)
         buffer_r.append(r/10)    # normalize reward, find to be useful
         s = s_
         ep_reward += r
-        # print(s,a,r,s_)
+        plot_x.append(s_[0])
+        plot_y.append(s_[1])
+        plot_z.append(s_[2])
         # update ppo
         if (j+1) % BATCH == 0 or j == MAX_EP_STEPS-1:
-            train=1
             v_s_ = ppo.get_v(s_)
             discounted_r = []
             for r in buffer_r[::-1]:
@@ -159,25 +199,27 @@ for i in range(MAX_EPISODES):
             bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
             buffer_s, buffer_a, buffer_r = [], [], []
             ppo.update(bs, ba, br)
+            # print("train")
 
         if j == MAX_EP_STEPS - 1:
             EWMA_step[0,i+1]=EWMA_p*EWMA_step[0,i]+(1-EWMA_p)*j
             EWMA_reward[0,i+1]=EWMA_p*EWMA_reward[0,i]+(1-EWMA_p)*ep_reward
-            print(ep_reward)
-            print('Episode:', i, ' Reward: %i' % int(ep_reward),"good","EWMA_step = ",EWMA_step[0,i+1],"EWMA_reward = ",EWMA_reward[0,i+1],"Train",train)
+            # print('Episode:', i, 'step', j, ' Reward: %i' % int(ep_reward), "EWMA_step = ", EWMA_step[0, i + 1],
+            #       "EWMA_reward = ", EWMA_reward[0, i + 1], s_[0], s_[1], s_[2])
             break
         #
-        # elif done:
-        #     EWMA_step[0,i+1]=EWMA_p*EWMA_step[0,i]+(1-EWMA_p)*j
-        #     EWMA_reward[0,i+1]=EWMA_p*EWMA_reward[0,i]+(1-EWMA_p)*ep_reward
-        #     if hit==1:
-        #         print('Episode:', i, ' Reward: %i' % int(ep_reward), "break in : ", j, "due to ",
-        #               "hit the wall", "EWMA_step = ", EWMA_step[0, i + 1], "EWMA_reward = ", EWMA_reward[0, i + 1],"Train",train)
-        #     break
+        elif done:
+            EWMA_step[0,i+1]=EWMA_p*EWMA_step[0,i]+(1-EWMA_p)*j
+            EWMA_reward[0,i+1]=EWMA_p*EWMA_reward[0,i]+(1-EWMA_p)*ep_reward
+            #
+            # print('Episode:', i, 'step', j, ' Reward: %i' % int(ep_reward), "EWMA_step = ", EWMA_step[0, i + 1],
+            #           "EWMA_reward = ", EWMA_reward[0, i + 1], s_[0], s_[1], s_[2])
+            break
 
     if i == 0: all_ep_r.append(ep_reward)
     else: all_ep_r.append(all_ep_r[-1]*0.9 + ep_reward*0.1)
-    print(
+    print('Episode:', i,'Stop at :',s_[0], s_[1], s_[2],
+          'step',j,
         'Ep: %i' % i,
         "|Ep_r: %i" % ep_reward,
         ("|Lam: %.4f" % METHOD['lam']) if METHOD['name'] == 'kl_pen' else '',
--- QUADROTOR_SIM.py
+++ QUADROTOR_SIM.py
@@ -1,11 +1,16 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import math
 import gym
 from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
 from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import Axes3D
+import time
 
-class QUADROTOR():
+class QUADROTOR(gym.Env):
     """
     Description:
         A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The pendulum starts upright, and the goal is to prevent it from falling over by increasing and reducing the cart's velocity.
@@ -72,43 +77,60 @@ class QUADROTOR():
         self.arm_length = L
 
         self.max_angle = 40 * math.pi/180 # you can specify the maximum commanded angle here
-        self.max_F = 2.5 * self.m * self.g # left these untouched from the nano plus
-        self.min_F = 0.05 * self.m * self.g # left these untouched from the nano plus
+        self.max_F = 2.5 * m * self.g # left these untouched from the nano plus
+        self.min_F = 0.05 * m * self.g # left these untouched from the nano plus
 
         # You can add any fields you want in self
         # for example you can add your controller gains by
         # self.k = 0, and they will be passed into controller.
 
-################以下内容还未修改，不过暂时不用到
-        self.force_mag = 20
-        self.tau = 0.02  # seconds between state updates
-        self.kinematics_integrator = 'euler'
+        x_bound = 5
+        y_bound = 5
+        z_bound = 5
+        xdot_bound = 5
+        ydot_bound = 5
+        zdot_bound = 5
+        qW_bound = 5
+        qX_bound = 5
+        qY_bound = 5
+        qZ_bound = 5
+        p_bound = 5
+        q_bound = 5
+        r_bound = 5
+        high_s = np.array([
+            x_bound*1.5,
+            y_bound*1.5,
+            z_bound*1.5,
+            xdot_bound,
+            ydot_bound,
+            zdot_bound,
+            qW_bound,
+            qX_bound,qY_bound,
+            qZ_bound,
+            p_bound,
+            q_bound,
+            r_bound,])
+        high_a = np.array([
+            self.max_F,
+            1,
+            1,
+            1,
+        ])
+        low_a = np.array([
+            self.min_F,
+            -1,
+            -1,
+            -1,
+        ])
 
-        # Angle at which to fail the episode
-        self.theta_threshold_radians = 20 * 2 * math.pi / 360
-        # self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 5
-        # self.max_v=1.5
-        # self.max_w=1
-        # FOR DATA
-        self.max_v = 50
-        self.max_w = 50
-
-        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
-        high = np.array([
-            self.x_threshold * 2,
-            self.max_v,
-            self.theta_threshold_radians * 2,
-            self.max_w])
-
-        self.action_space = spaces.Box(low=-self.force_mag, high=self.force_mag, shape=(1,), dtype=np.float32)
-        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+        self.force_space = spaces.Box(low=low_a, high=high_a, dtype=np.float32)
+        self.observation_space = spaces.Box(-high_s, high_s, dtype=np.float32)
 
         self.seed()
         self.viewer = None
         self.state = None
-
         self.steps_beyond_done = None
+        self.desire_state = None
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -135,7 +157,7 @@ class QUADROTOR():
                   [0.25, -0.5 / self.arm_length, 0]]
 
 
-        prop_thrusts = np.dot(self.A, [[F], M[0], M[1]])
+        prop_thrusts = np.dot(self.A, [F, M[0], M[1]])
 
         prop_thrusts_clamped = np.maximum(np.minimum(prop_thrusts, self.max_F / 4), self.min_F / 4)
         B = [[1, 1, 1, 1],
@@ -197,18 +219,109 @@ class QUADROTOR():
         sdot[12] = pqrdot[2]
         return sdot
 
-    def step(self, F,M):
-        time = np.linspace(0, 0.01, 2)
-        s= self.state
+    def step_old(self, F,M):
+        time = np.linspace(0, 0.001, 2)
+        s = self.state
         s_ = odeint(self.quadEOM, s, time, args=(F, M), tfirst=True)
+        self.state = s_[1]
+
+        return self.state
+
+    def ode_step(self,F,M):
+        # print(F,M)
+        s_dot = self.quadEOM(0, self.state, F, M)
+        # print(s_dot)
+        s_ = self.state + 0.005 * s_dot
+        self.state = s_
+        return self.state
+
+    def step(self,t):
+        time = np.linspace(t,t+0.001,2)
+        s= self.state
+        s_ = odeint(self.equation,s, time,args=(traj,),tfirst=True)
         self.state=s_[1]
+
+        return self.state
+
+    def reset_traj(self,start, yaw):
+        s = np.zeros([13])
+        phi0 = 0.0
+        theta0 = 0.0
+        psi0 = yaw
+        Rot0 = RPYtoRot_ZXY(phi0, theta0, psi0)
+        Rot0=np.array(Rot0)
+        Quat0 = RotToQuat(Rot0)
+        s[0] = start[0]   # x
+        s[1] = start[1]   # y
+        s[2] = start[2]   # z
+        s[3] = 0          # xdot
+        s[4] = 0          # ydot
+        s[5] = 0          # zdot
+        s[6] = Quat0[0]   # qw
+        s[7] = Quat0[1]   # qx
+        s[8] = Quat0[2]   # qy
+        s[9] = Quat0[3]  # qz
+        s[10] = 0         # p
+        s[11] = 0         # q
+        s[12] = 0         # r
+        self.state = s
+        self.steps_beyond_done = None
         return self.state
 
     def reset(self):
-        self.state = self.np_random.uniform(low=-0.2, high=0.2, size=(4,))
+        self.state = np.zeros([13])
         self.state[0] = self.np_random.uniform(low=-2, high=2)
-        self.steps_beyond_done = None
-        return np.array(self.state)
+        self.state[1] = self.np_random.uniform(low=-2, high=2)
+        self.state[3] = self.np_random.uniform(low=2, high=4)
+        self.state[6] = 1
+        return self.state
+
+    def controller(self,desired_state,x):
+        # CONTROLLER quadrotor controller
+        # The current states are:
+        # pos, vel, euler = [roll;pitch;yaw], qd{qn}.omega
+        # The desired states are:
+        # pos_des, vel_des, acc_des, yaw_des, yawdot_des
+        # Using these current and desired states, you have to compute the desired controls% position controller params
+
+        # position controller params
+        Kp = [15,15,30]
+        Kd = [12,12,10]
+
+        # attitude controller params
+        KpM = np.ones([3])*3000
+        KdM = np.ones([3])*300
+
+        # desired_state=[pos,vel,acc,yaw,yawdot]
+        # x y z xdot ydot zdot qw qx qy qz p q r
+        [pos, vel, euler, omega]=stateToQd(x)
+        pos_des,vel_des,acc_des,yaw_des,yawdot_des=desired_state[0],desired_state[1],desired_state[2],desired_state[3],desired_state[4]
+
+
+        acc_des = acc_des + Kd*(np.subtract(vel_des,vel)) + Kp*(np.subtract(pos_des,pos))
+        #
+        #  Desired roll, pitch and yaw
+        phi_des = 1/self.g * (acc_des[0]*np.sin(yaw_des) - acc_des[1]*np.cos(yaw_des))
+        theta_des = 1/self.g * (acc_des[0]*np.cos(yaw_des) + acc_des[1]*np.sin(yaw_des))
+        psi_des = yaw_des
+        #
+        euler_des = [phi_des,theta_des,psi_des]
+        pqr_des = [0,0,yawdot_des]
+
+        # Thurst
+        # qd{qn}.acc_des(3);
+        F  = self.m*(self.g + acc_des[2])
+        # Moment
+
+        M_=np.multiply(KdM,(np.subtract(pqr_des,omega))) + np.multiply(KpM,(np.subtract(euler_des,euler)))
+        M =  np.dot(self.I,M_)
+        return F,M
+
+    def equation(self,t,x,traj):
+        desired_state = trajectory(traj,t)
+        F, M = self.controller(desired_state,x)
+        s_dot =self.quadEOM(t,x,F,M)
+        return s_dot
 
 
 
@@ -225,7 +338,7 @@ def stateToQd(x):
 
     euler = [phi,theta,yaw]
     omega = x[10:13]
-    return [pos,vel,euler,omega]
+    return [pos.tolist(),vel.tolist(),euler,omega.tolist()]
 def QuatToRot(q):
     # QuatToRot Converts a Quaternion to Rotation matrix
     # normalize q
@@ -270,12 +383,230 @@ def RotToRPY_ZXY(R):
     psi = math.atan2(-R[1, 0] / math.cos(phi), R[1, 1] / math.cos(phi))
     theta = math.atan2(-R[0, 2] / math.cos(phi), R[2, 2] / math.cos(phi))
     return [phi,theta,psi]
+def RotToQuat(R):
+    # ROTTOQUAT Converts a Rotation matrix into a Quaternion
+    # takes in W_R_B rotation matrix
+    tr = R[0,0] + R[1,1] + R[2,2]
 
+    if (tr > 0):
+      S = np.sqrt(tr+1.0) * 2 # S=4*qw
+      qw = 0.25 * S
+      qx = (R[2,1] - R[1,2]) / S
+      qy = (R[0,2] - R[2,0]) / S
+      qz = (R[1,0] - R[0,1]) / S
 
-x0=[1.5,12.,144.,12.,14.,15.,12.,1.,13.,15.,13.,12.,1.]
-F=2
-M=[[3],[2.5],[1]]
+    elif ((R[0,0] > R[1,1]) and (R[0,0] > R[2,2])):
+      S = np.sqrt(1.0 + R[0,0] - R[1,1] - R[2,2]) * 2#  S=4*qx
+      qw = (R[2,1] - R[1,2]) / S
+      qx = 0.25 * S
+      qy = (R[0,1] + R[1,0]) / S
+      qz = (R[0,2] + R[2,0]) / S
+
+    elif (R[1,1] > R[2,2]):
+      S = np.sqrt(1.0 + R[1,1] - R[0,0] - R[2,2]) * 2#  S=4*qy
+      qw = (R[0,2] - R[2,0]) / S
+      qx = (R[0,1] + R[1,0]) / S
+      qy = 0.25 * S
+      qz = (R[1,2] + R[2,1]) / S
+    else:
+      S = np.sqrt(1.0 + R[2,2] - R[0,0] - R[1,1]) * 2#  S=4*qz
+      qw = (R[1,0] - R[0,1]) / S
+      qx = (R[0,2] + R[2,0]) / S
+      qy = (R[1,2] + R[2,1]) / S
+      qz = 0.25 * S
+
+    q = [qw,qx,qy,qz]
+    q = np.multiply(q,np.sign(qw))
+    return q
+# Output desired_state
+def trajectory(name,t):
+    if name == 'circle':
+        time_tol = 12
+        radius = 5
+        dt = 0.0001
+        def tj_from_line(start_pos, end_pos, time_ttl, t_c):
+            v_max = (end_pos - start_pos) * 2 / time_ttl
+            if t_c >= 0 and t_c < time_ttl / 2:
+                vel = v_max * t_c / (time_ttl / 2)
+                pos = start_pos + t_c * vel / 2
+                acc = [0, 0, 0]
+            else:
+                vel = v_max * (time_ttl - t_c) / (time_ttl / 2)
+                pos = end_pos - (time_ttl - t_c) * vel / 2
+                acc = [0, 0, 0]
+            return [pos, vel, acc[0], acc[1], acc[2]]
+
+        def pos_from_angle(a):
+            # pos = [radius*np.cos(a), radius*np.sin(a), 2.5*a/(2*np.pi)]
+            pos = [np.multiply(radius, np.cos(a)), np.multiply(radius, np.sin(a)), np.multiply(2.5 / (2 * np.pi), a)]
+            return pos
+
+        def get_vel(t):
+            angle1 = tj_from_line(0, 2 * np.pi, time_tol, t)[0]
+            pos1 = pos_from_angle(angle1)
+            angle2 = tj_from_line(0, 2 * np.pi, time_tol, t + dt)[0]
+            pos2 = pos_from_angle(angle2)
+            vel = (np.subtract(pos2, pos1)) / dt
+            return vel
+
+        if t > time_tol:
+            pos = [radius, 0, 2.5]
+            vel = [0, 0, 0]
+            acc = [0, 0, 0]
+        else:
+            angle = tj_from_line(0, 2 * np.pi, time_tol, t)[0]
+            pos = pos_from_angle(angle)
+            vel = get_vel(t).tolist()
+            acc = ((get_vel(t + dt) - get_vel(t)) / dt).tolist()
+        yaw = 0
+        yawdot = 0
+
+        desired_state = [pos, vel, acc, yaw, yawdot]
+    elif name=='hover':
+        time_tol = 1000
+        length = 5
+        if t <= 0:
+            pos = [0, 0, 0]
+            vel = [0, 0, 0]
+            acc = [0, 0, 0]
+        else:
+            pos = [1, 0, 0]
+            vel = [0, 0, 0]
+            acc = [0, 0, 0]
+
+        yaw = 0
+        yawdot = 0
+
+        desired_state = [pos, vel, acc, yaw, yawdot]
+    return desired_state
+
 env=QUADROTOR()
-env.state=x0
-s_=env.step(F,M)
-print(s_)
+traj='circle'
+des_start = trajectory(traj,0)
+des_stop = trajectory(traj,100)
+state=env.reset_traj(des_start[0],0)
+# print(state)
+# state=env.reset()
+# print(state)
+
+t=0
+cstep     = 0.001
+fig = plt.figure()
+ax = plt.subplot(projection='3d')  # 创建一个三维的绘图工程
+plot_x=[]
+plot_y=[]
+plot_z=[]
+d_x=[]
+d_y=[]
+d_z=[]
+t1 = time.time()
+# x y z xdot ydot zdot qw qx qy qz p q r
+# for step in range(1500):
+#     desired_state = trajectory(traj, t)
+#     s_=env.step(t,traj)
+#     t=t+0.01
+#     print(step,s_[0],s_[1],s_[2])
+#     plot_x.append(s_[0])
+#     plot_y.append(s_[1])
+#     plot_z.append(s_[2])
+#     d_x.append(desired_state[0][0])
+#     d_y.append(desired_state[0][1])
+#     d_z.append(desired_state[0][2])
+#     # ax.scatter(plot_x, plot_y, plot_z, c='r')  # 绘制数据点,颜色是红色
+#     # ax.set_zlabel('Z')  # 坐标轴
+#     # ax.set_ylabel('Y')
+#     # ax.set_xlabel('X')
+#     # plt.draw()
+#     # plt.pause(0.000001)
+# ax.scatter(plot_x, plot_y, plot_z, c='r')  # 绘制数据点,颜色是红色
+# ax.scatter(d_x, d_y, d_z, c='b')
+# ax.set_zlabel('Z')  # 坐标轴
+# ax.set_ylabel('Y')
+# ax.set_xlabel('X')
+# plt.draw()
+# plt.pause(10000)
+# plt.savefig('3D.jpg')
+# plt.close()
+M1=0
+M2=0
+M3=0
+S0=0
+S1=0
+S2=0
+S3=0
+S4=0
+S5=0
+S6=0
+S7=0
+S8=0
+S9=0
+S10=0
+S11=0
+S12=0
+S13=0
+for step in range(15000):
+    desired_state=trajectory(traj,t)
+    F,M=env.controller(desired_state,env.state)
+    print(M)
+    # print(time.time() - t1)
+    s_=env.ode_step(F,M)
+    # print(time.time() - t1)
+    t=t+0.005
+    if abs(M[0])>M1:
+        M1=abs(M[0])
+    if abs(M[1])>M2:
+        M2=abs(M[1])
+    if abs(M[2])>M3:
+        M3=abs(M[2])
+    # print(M1,M2,M3)
+    # #
+    # if abs(s_[0])>S0:
+    #     S0=abs(s_[0])
+    # if abs(s_[1])>S1:
+    #     S1=abs(s_[1])
+    # if abs(s_[2])>S2:
+    #     S2=abs(s_[2])
+    # if abs(s_[3])>S3:
+    #     S3=abs(s_[3])
+    # if abs(s_[4])>S4:
+    #     S4=abs(s_[4])
+    # if abs(s_[5])>S5:
+    #     S5=abs(s_[5])
+    # if abs(s_[6])>S6:
+    #     S6=abs(s_[6])
+    # if abs(s_[7])>S7:
+    #     S7=abs(s_[7])
+    # if abs(s_[8])>S8:
+    #     S8=abs(s_[8])
+    # if abs(s_[9])>S9:
+    #     S9=abs(s_[9])
+    # if abs(s_[10])>S10:
+    #     S10=abs(s_[10])
+    # if abs(s_[11])>S11:
+    #     S11=abs(s_[11])
+    # if abs(s_[12])>S12:
+    #     S12=abs(s_[12])
+    # S=[S0,S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12]
+    # print(step,s_[0],s_[1],s_[2])
+    # print(S)
+    d_x.append(desired_state[0][0])
+    d_y.append(desired_state[0][1])
+    d_z.append(desired_state[0][2])
+    plot_x.append(s_[0])
+    plot_y.append(s_[1])
+    plot_z.append(s_[2])
+    # ax.scatter(plot_x, plot_y, plot_z, c='r')  # 绘制数据点,颜色是红色
+    # ax.set_zlabel('Z')  # 坐标轴
+    # ax.set_ylabel('Y')
+    # ax.set_xlabel('X')
+    # plt.draw()
+    # plt.pause(0.000001)
+ax.scatter(plot_x, plot_y, plot_z, c='r')  # 绘制数据点,颜色是红色
+ax.scatter(d_x, d_y, d_z, c='b')
+ax.set_zlabel('Z')  # 坐标轴
+ax.set_ylabel('Y')
+ax.set_xlabel('X')
+plt.draw()
+plt.pause(10000)
+plt.savefig('3D.jpg')
+plt.close()
